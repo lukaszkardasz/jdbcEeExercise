d60201624c4b28f6baf2bc554f6d71c4db5ff1c6
lukaszkardasz <kardasz.lukasz@gmail.com>
2019-12-16 14:38:25 +0100

    little fix and connect block

--- src/pl/n2god/data/City.java
+++ src/pl/n2god/data/City.java
@@ -6,7 +6,7 @@ package pl.n2god.data;
  */
 public class City {
 	private String name;
-	private String population;
+	private int population;
 
 	public String getName() {
 		return name;
@@ -16,18 +16,18 @@ public class City {
 		this.name = name;
 	}
 
-	public String getPopulation() {
+	public int getPopulation() {
 		return population;
 	}
 
-	public void setPopulation(String population) {
+	public void setPopulation(int population) {
 		this.population = population;
 	}
 
 	public City() {
 	}
 
-	public City(String name, String population) {
+	public City(String name, int population) {
 		this.name = name;
 		this.population = population;
 	}
--- src/pl/n2god/servlet/SqlServlet.java
+++ src/pl/n2god/servlet/SqlServlet.java
@@ -1,11 +1,17 @@
 package pl.n2god.servlet;
 
+import pl.n2god.data.City;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Result;
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author n2god on 16/12/2019
@@ -16,5 +22,40 @@ public class SqlServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String param = request.getParameter("get");
+		if ("show".equals(param)) {
+			List<City> cityList = getCities();
+			request.setAttribute("cityList", cityList);
+			request.getRequestDispatcher("cityList.jsp").forward(request, response);
+		} else {
+			response.sendError(403);
+		}
+	}
+
+	private List<City> getCities() throws ClassNotFoundException {
+		final String driver = "com.mysql.jf.jdbc.Driver";
+		Class.forName(driver);
+
+		List<City> cityList = null;
+		final String dbPath = "jdbc:mysql://localhost:3306/world?serverTimezone=UTC";
+		final String sqlQuery = "SELECT Name, Population FROM city";
+
+		try (Connection connection = DriverManager.getConnection(dbPath, "user", "password");
+		     Statement statement = connection.createStatement();
+		     ResultSet resultSet = statement.executeQuery(sqlQuery))
+		{
+			String cityName = null;
+			int cityPopulation = 0;
+			cityList = new ArrayList<>();
+			while (resultSet.next()){
+				cityName = resultSet.getString("Name");
+				cityPopulation = resultSet.getInt("Population");
+				City city = new City(cityName, cityPopulation);
+			}
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
